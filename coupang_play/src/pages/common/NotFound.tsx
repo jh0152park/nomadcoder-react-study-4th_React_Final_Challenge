93145cc7ee81aa9bb63120a923f306d456783dfb
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-18 22:59:16 +0900

    added not found component

--- coupang_play/src/pages/common/NotFound.tsx
+++ coupang_play/src/pages/common/NotFound.tsx
@@ -1,3 +1,31 @@
+import { Center, Heading, Image, VStack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
-    return null;
+    const naviage = useNavigate();
+    return (
+        <Center w="100%" h="100vh">
+            <VStack>
+                <Image
+                    w="300px"
+                    src="/resources/images/common/logo.webp"
+                    mb="50px"
+                />
+                <Heading>404 Page Not Found</Heading>
+                <Heading
+                    _hover={{
+                        background:
+                            "linear-gradient(to right top, rgb(255, 147, 242), rgb(97, 208, 255))",
+                        cursor: "pointer",
+                        color: "transparent",
+                        WebkitBackgroundClip: "text",
+                    }}
+                    transition="all 0.2s linear"
+                    onClick={() => naviage("/")}
+                >
+                    Click to Home
+                </Heading>
+            </VStack>
+        </Center>
+    );
 }
