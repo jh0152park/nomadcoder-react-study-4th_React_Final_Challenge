3a9b5ade07ecfe4a8556fab6097b7fa5d447a63b
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-18 20:56:02 +0900

    added global styles

--- coupang_play/src/index.tsx
+++ coupang_play/src/index.tsx
@@ -1,7 +1,23 @@
+import { reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import ReactDOM from "react-dom/client";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    background-color: #121212;
+  }
+`;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+root.render(
+    <RecoilRoot>
+        <GlobalStyle />
+        <App />
+    </RecoilRoot>
+);
