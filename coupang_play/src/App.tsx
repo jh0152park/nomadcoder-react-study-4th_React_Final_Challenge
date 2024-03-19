7a633aa231f63ee779b5f0d3f1ca31e4a15ffbff
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-20 00:33:08 +0900

    added changed bg header

--- coupang_play/src/App.tsx
+++ coupang_play/src/App.tsx
@@ -3,6 +3,8 @@ import Layout from "./pages/common/Layout";
 import NotFound from "./pages/common/NotFound";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import HomeLayout from "./pages/HomeLayout";
+import { Suspense } from "react";
 
 const router = createBrowserRouter([
     {
@@ -14,8 +16,19 @@ const router = createBrowserRouter([
                 path: "",
                 element: <Login />,
             },
+        ],
+    },
+    {
+        path: "/home",
+        element: (
+            <Suspense fallback={<h1>Loading~.~</h1>}>
+                <HomeLayout />
+            </Suspense>
+        ),
+        errorElement: <NotFound />,
+        children: [
             {
-                path: "/home",
+                path: "",
                 element: <Home />,
             },
         ],
--- coupang_play/src/global/projectCommon.ts
+++ coupang_play/src/global/projectCommon.ts
@@ -17,3 +17,26 @@ export const USER_NAME = atom({
     default: "",
     effects_UNSTABLE: [persistAtom],
 });
+
+type TCategory =
+    | "TV"
+    | "영화"
+    | "스포츠"
+    | "스토어"
+    | "키즈"
+    | "뉴스"
+    | "찜한 콘텐츠";
+
+type TCategories = {
+    [key in TCategory]: string;
+};
+
+export const CATEGORIES_LINK: TCategories = {
+    TV: "tv",
+    영화: "movies",
+    스포츠: "live",
+    스토어: "buy",
+    키즈: "kids",
+    뉴스: "news",
+    "찜한 콘텐츠": "mylist",
+};
