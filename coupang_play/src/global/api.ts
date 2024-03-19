258b0faed99422de1e5ffa0d8a6e2d5b4e43013f
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-19 23:13:37 +0900

    added login modal

--- coupang_play/src/global/api.ts
+++ coupang_play/src/global/api.ts
@@ -17,16 +17,29 @@ export interface IUserHandlerResponse {
     issuccess: boolean;
 }
 
+export interface IModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export interface IUserInputFormProps {
+    name: string;
+    email: string;
+}
+
 export class UserHandlerAPI {
     public async register({ email, name }: IRegisterProps) {
         return (
             await axios.get(
-                `${process.env.REACT_APP_USER_DB_URL}/register?email=${email}&name=${name}`,
-                {
-                    headers: {
-                        "X-CSRFToken": Cookie.get("csrftoken") || "",
-                    },
-                }
+                `${process.env.REACT_APP_USER_DB_URL}/register?email=${email}&name=${name}`
+            )
+        ).data;
+    }
+
+    public async login({ email }: ILoginProps) {
+        return (
+            await axios.get(
+                `${process.env.REACT_APP_USER_DB_URL}/account?email=${email}`
             )
         ).data;
     }
