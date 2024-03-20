e2b6edb2e1cbe237442c529471954f2d684fecc0
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-20 16:53:38 +0900

    added tv apis

--- coupang_play/src/global/api.ts
+++ coupang_play/src/global/api.ts
@@ -97,4 +97,179 @@ export class MovieHandlerAPI {
             options
         ).then((response) => response.json());
     }
+
+    public async details({ queryKey }: QueryFunctionContext) {
+        const [_, id] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+            options
+        ).then((response) => response.json());
+    }
+
+    // actors list check
+    public async credits({ queryKey }: QueryFunctionContext) {
+        const [_, id] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`,
+            options
+        ).then((response) => response.json());
+    }
+
+    // official videos check
+    public async videos({ queryKey }: QueryFunctionContext) {
+        const [_, id] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+            options
+        ).then((response) => response.json());
+    }
+}
+
+/* eslint-disable */
+export class TVHandlerAPI {
+    public async airingToday({ queryKey }: QueryFunctionContext) {
+        const [_, page] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=${page}`,
+            options
+        ).then((response) => response.json());
+    }
+
+    public async onTheAir({ queryKey }: QueryFunctionContext) {
+        const [_, page] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=${page}`,
+            options
+        ).then((response) => response.json());
+    }
+
+    public async popular({ queryKey }: QueryFunctionContext) {
+        const [_, page] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/popular?language=en-US&page=${page}`,
+            options
+        ).then((response) => response.json());
+    }
+
+    public async topRated({ queryKey }: QueryFunctionContext) {
+        const [_, page] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=${page}`,
+            options
+        ).then((response) => response.json());
+    }
+
+    public async details({ queryKey }: QueryFunctionContext) {
+        const [_, id] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/${id}?language=en-US`,
+            options
+        ).then((response) => response.json());
+    }
+
+    public async credits({ queryKey }: QueryFunctionContext) {
+        const [_, id] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/${id}/credits?language=en-US`,
+            options
+        ).then((response) => response.json());
+    }
+
+    public async videos({ queryKey }: QueryFunctionContext) {
+        const [_, id] = queryKey;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzlhNzA4ODRhMTY2ZDQ2ODE0ZTYwNTMwMTU2OGQyZCIsInN1YiI6IjY0YmEyMDIwMzAwOWFhMDBlMjY0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eHILwGMMFNYioeUQaVUSgf_kU4PRKZP99iDQOHXQRpI",
+            },
+        };
+
+        return fetch(
+            `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`,
+            options
+        ).then((response) => response.json());
+    }
 }
