8f5c4e5627f8813c2e0e88951e9018412a483d05
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-22 15:26:17 +0900

    added copy link function

--- coupang_play/src/components/detail/ShareButton.tsx
+++ coupang_play/src/components/detail/ShareButton.tsx
@@ -1,7 +1,24 @@
-import { Center, Icon, Text, VStack } from "@chakra-ui/react";
+import { Center, Icon, Text, VStack, useToast } from "@chakra-ui/react";
 import { IoMdShare } from "react-icons/io";
 
 export default function ShareButton() {
+    const toast = useToast();
+
+    async function handleURLCopyToClipBoard() {
+        try {
+            await navigator.clipboard.writeText(window.location.href);
+            toast({
+                status: "success",
+                title: "링크가 복사되었습니다",
+            });
+        } catch {
+            toast({
+                status: "error",
+                title: "링크복사에 실패했습니다",
+            });
+        }
+    }
+
     return (
         <VStack
             ml="25px"
@@ -9,6 +26,7 @@ export default function ShareButton() {
             _hover={{
                 cursor: "pointer",
             }}
+            onClick={handleURLCopyToClipBoard}
         >
             <Center
                 w="30px"
